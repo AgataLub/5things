cb1fba05f30eeb21d837ef31b6455f4afda9781c
Agata Lubanska <swistaczek@Agatas-MacBook-Pro.local>
2020-10-13 10:32:31 +0200

    push

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", loadScreen);
+
+var zIndex = 1;
+
+function bringFront() {
+    console.log("bringFront");
+
+    document.querySelector("#one").style.zIndex = zIndex + "+1";
+}
+
+function loadScreen() {
+    console.log("loadScreen");
+
+    document.querySelector("#one").addEventListener("click", bringFront);
+    document.querySelector("#two").addEventListener("click", bringFront);
+    document.querySelector("#three").addEventListener("click", bringFront);
+    document.querySelector("#four").addEventListener("click", bringFront);
+    document.querySelector("#five").addEventListener("click", bringFront);
+}
